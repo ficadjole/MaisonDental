2c0d56da6cf12524774fff3052a048e1c205f5c2
unknown <velemirfilip@gmail.com>
2024-04-01 12:22:53 +0200

    Nesto

---
+++ java.js
@@ -0,0 +1,11 @@
+const modal = document.querySelector('#modal');
+const openModel = document.querySelector('.open-button');
+const closeModel = document.querySelector('.close-button');
+
+openModel.addEventListener('click',() => {
+  modal.showModal();
+})
+
+closeModel.addEventListener('click',() => {
+  modal.close();
+})
