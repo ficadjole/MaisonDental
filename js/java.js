c5df77fcd4080feea41f15f0be775d76010a47ff
unknown <velemirfilip@gmail.com>
2024-04-24 23:01:58 +0200

    izmene

--- js/java.js
+++ js/java.js
@@ -1,8 +1,13 @@
 const modal = document.querySelector("#modal");
 const openModel = document.querySelector(".open-button");
 const closeModel = document.querySelector(".close-button");
+const openMdl = document.querySelector(".open-btn");
 
 openModel.addEventListener("click", () => {
+  modal.showModal();
+});
+
+openMdl.addEventListener("click", () => {
   modal.showModal();
 });
 
